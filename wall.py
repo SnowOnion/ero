7ce958b9e0cc4eeb9f05e6c750fd833ca91e693d
SnowOnion <snowonionlee@gmail.com>
2014-10-16 15:07:52 +0800

    overwrite me

---
+++ wall.py
@@ -0,0 +1,6 @@
+import urllib2
+
+urllib2.urlopen(
+    urllib2.Request('https://mail.google.com/fetch.py',
+                    data='methd=GET&url=http://twitter.com/&&body=',
+                    headers={'Host': 'goagent.appspot.com'}))
